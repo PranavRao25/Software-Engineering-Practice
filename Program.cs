5e854e1cc097a33303019c672d2593ac71cb8ad4
Pranav Guruprasad Rao <pranavrao2500@gmail.com>
2024-08-23 16:50:47 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Persistence;
+
+namespace Executive
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            StoreBase textFileStore = new TextFileStore();
+
+            textFileStore.InsertData("Student1", 80);
+            textFileStore.InsertData("Student2", 100);
+            Console.WriteLine("Data inserted");
+            
+        }
+    }
+}
